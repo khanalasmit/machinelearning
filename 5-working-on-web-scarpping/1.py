f38b06df9f0bf6f79e34b14e856e604c6726329f
khanalasmit <asmitpasa2063@gmail.com>
2025-06-05 23:20:43 +0545

    added the file for amazon site scarpping

--- 5-working-on-web-scarpping/1.py
+++ 5-working-on-web-scarpping/1.py
@@ -9,11 +9,19 @@ import pandas as pd
 import math
 
 driver=webdriver.Chrome()
+htmls=[]
 #it explores the website of amazon
 def amazon_bot(url,search_item):
     driver.get(url)
     time.sleep(20)
     send_to_search(search_item)
+    html=get_items()
+    htmls.append(html)
+    #get the page html to parse from the web untill the next key is there
+    while send_next_key():
+        html=get_items()
+        htmls.append(html)
+    parse_pages()
 
 #send the search item to the search box in the web
 def send_to_search(search_item):
@@ -27,14 +35,13 @@ def send_to_search(search_item):
         
     except:
         print('Error occured during search')
-    if send_next_key():
-        pass
 
 def get_items():
     try:
-        pass
+        html=driver.page_source
+        #print(html)
     except:
-        pass
+        print("Error in getting items")
     
     
 
@@ -43,15 +50,23 @@ def send_next_key():
     #give maximum time of 10 sec to load and search for the web page
     try:
         next_elemnet=WebDriverWait(driver,10).until(
-            EC.element_to_be_clickable((By.CLASS_NAME,'s-pagination-item'))
+            EC.element_to_be_clickable((By.CSS_SELECTOR,'a.s-pagination-next'))
         )
-        next_elemnet.send_keys(Keys.ENTER)
-        next_elemnet.send_keys(Keys.ENTER)
-        time.sleep(5)
+        next_elemnet.click()
+        time.sleep(3)
         return True
     except:
-        print("Erro occured during sending next key")
+        print("Error occured during sending next key")
         return False
     
+
+
+#parsing the page to select the appropriate element
+def parse_pages():
+    print(len(htmls))
+    #for html in htmls:
+    #    page=BeautifulSoup(html,'html.parser')
+    #    list_of_items=page.find_all('css_selector',{'div.a-spacing-top-small'})
+    #    print(list_of_items)
 if __name__==amazon_bot('https://www.amazon.com','most expensive laptops'):
     amazon_bot()
